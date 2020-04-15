7538a971db85ecf33cde6ac5cbf6999fca57e0fc
Oleksiy Maksumchyk <lykabraine27@gmail.com>
2020-04-15 13:53:07 +0300

    color draw was worked

--- tic_tac_toe/draw.cpp
+++ tic_tac_toe/draw.cpp
@@ -3,6 +3,25 @@
 #include <windows.h>
 using namespace std;
 
+enum ConsoleColor {
+	Black = 0,
+	Blue = 1,
+	Green = 2,
+	Cyan = 3,
+	Red = 4,
+	Magenta = 5,
+	Brown = 6,
+	LightGray = 7,
+	DarkGray = 8,
+	LightBlue = 9,
+	LightGreen = 10,
+	LightCyan = 11,
+	LightRed = 12,
+	LightMagenta = 13,
+	Yellow = 14,
+	White = 15
+};
+
 void Draw(char** newMap)
 {
 	const int SIZE = 5;
@@ -28,11 +47,13 @@ void Draw(char** newMap)
 	system("cls");
 	system("color 0");
 
+	newMap[0][0] = 'x';
+
 	for (size_t i = 0; i < 15; i++)
 	{
 		if (i == 0 || i == 5 || i == 10)
 		{
-			//SetConsoleTextAttribute(hConsole, (WORD)(White));
+			SetConsoleTextAttribute(hConsole, (WORD)(White));
 			for (int i = 0; i < 15 + 4; i++)
 			{
 				cout << " #";
@@ -43,19 +64,19 @@ void Draw(char** newMap)
 		{
 			if (j == 0 || j == 5 || j == 10)
 			{
-				//SetConsoleTextAttribute(hConsole, (WORD)(White));
+				SetConsoleTextAttribute(hConsole, (WORD)(White));
 				cout << " #";
 			}
 			if ((i >= 0 && i < 5) && (j >= 0 && j < 5))
 			{
 				if (newMap[0][0] == 'x')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Red));
+					SetConsoleTextAttribute(hConsole, (WORD)(Red));
 					cout << " " << CROSS_IMAGE[i][j];
 				}
 				else if (newMap[0][0] == 'o')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Blue));
+					SetConsoleTextAttribute(hConsole, (WORD)(Blue));
 					cout << " " << NULL_IMAGE[i][j];
 				}
 				else
@@ -67,12 +88,12 @@ void Draw(char** newMap)
 			{
 				if (newMap[0][1] == 'x')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Red));
+					SetConsoleTextAttribute(hConsole, (WORD)(Red));
 					cout << " " << CROSS_IMAGE[i][j - 5];
 				}
 				else if (newMap[0][1] == 'o')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Blue));
+					SetConsoleTextAttribute(hConsole, (WORD)(Blue));
 					cout << " " << NULL_IMAGE[i][j - 5];
 				}
 				else
@@ -84,12 +105,12 @@ void Draw(char** newMap)
 			{
 				if (newMap[0][2] == 'x')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Red));
+					SetConsoleTextAttribute(hConsole, (WORD)(Red));
 					cout << " " << CROSS_IMAGE[i][j - 10];
 				}
 				else if (newMap[0][2] == 'o')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Blue));
+					SetConsoleTextAttribute(hConsole, (WORD)(Blue));
 					cout << " " << NULL_IMAGE[i][j - 10];
 				}
 				else
@@ -101,12 +122,12 @@ void Draw(char** newMap)
 			{
 				if (newMap[1][0] == 'x')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Red));
+					SetConsoleTextAttribute(hConsole, (WORD)(Red));
 					cout << " " << CROSS_IMAGE[i - 5][j];
 				}
 				else if (newMap[1][0] == 'o')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Blue));
+					SetConsoleTextAttribute(hConsole, (WORD)(Blue));
 					cout << " " << NULL_IMAGE[i - 5][j];
 				}
 				else
@@ -118,12 +139,12 @@ void Draw(char** newMap)
 			{
 				if (newMap[1][1] == 'x')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Red));
+					SetConsoleTextAttribute(hConsole, (WORD)(Red));
 					cout << " " << CROSS_IMAGE[i - 5][j - 5];
 				}
 				else if (newMap[1][1] == 'o')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Blue));
+					SetConsoleTextAttribute(hConsole, (WORD)(Blue));
 					cout << " " << NULL_IMAGE[i - 5][j - 5];
 				}
 				else
@@ -135,12 +156,12 @@ void Draw(char** newMap)
 			{
 				if (newMap[1][2] == 'x')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Red));
+					SetConsoleTextAttribute(hConsole, (WORD)(Red));
 					cout << " " << CROSS_IMAGE[i - 5][j - 10];
 				}
 				else if (newMap[1][2] == 'o')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Blue));
+					SetConsoleTextAttribute(hConsole, (WORD)(Blue));
 					cout << " " << NULL_IMAGE[i - 5][j - 10];
 				}
 				else
@@ -152,12 +173,12 @@ void Draw(char** newMap)
 			{
 				if (newMap[2][0] == 'x')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Red));
+					SetConsoleTextAttribute(hConsole, (WORD)(Red));
 					cout << " " << CROSS_IMAGE[i - 10][j];
 				}
 				else if (newMap[2][0] == 'o')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Blue));
+					SetConsoleTextAttribute(hConsole, (WORD)(Blue));
 					cout << " " << NULL_IMAGE[i - 10][j];
 				}
 				else
@@ -169,12 +190,12 @@ void Draw(char** newMap)
 			{
 				if (newMap[2][1] == 'x')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Red));
+					SetConsoleTextAttribute(hConsole, (WORD)(Red));
 					cout << " " << CROSS_IMAGE[i - 10][j - 5];
 				}
 				else if (newMap[2][1] == 'o')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Blue));
+					SetConsoleTextAttribute(hConsole, (WORD)(Blue));
 					cout << " " << NULL_IMAGE[i - 10][j - 5];
 				}
 				else
@@ -186,12 +207,12 @@ void Draw(char** newMap)
 			{
 				if (newMap[2][2] == 'x')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Red));
+					SetConsoleTextAttribute(hConsole, (WORD)(Red));
 					cout << " " << CROSS_IMAGE[i - 10][j - 10];
 				}
 				else if (newMap[2][2] == 'o')
 				{
-					//SetConsoleTextAttribute(hConsole, (WORD)(Blue));
+					SetConsoleTextAttribute(hConsole, (WORD)(Blue));
 					cout << " " << NULL_IMAGE[i - 10][j - 10];
 				}
 				else
@@ -201,13 +222,13 @@ void Draw(char** newMap)
 			}
 			if (j == 14)
 			{
-				//SetConsoleTextAttribute(hConsole, (WORD)(White));
+				SetConsoleTextAttribute(hConsole, (WORD)(White));
 				cout << " #";
 			}
 		}
 		if (i == 14)
 		{
-			//SetConsoleTextAttribute(hConsole, (WORD)(White));
+			SetConsoleTextAttribute(hConsole, (WORD)(White));
 			cout << endl;
 			for (int i = 0; i < 15 + 4; i++)
 			{
